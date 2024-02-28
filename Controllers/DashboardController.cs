49d7b6e2059027dd153073e0f3eef3f6b41ac91c
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-28 14:33:18 +0300

    committed

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KidKinder.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,17 @@ namespace KidKinder.Controllers
 {
     public class DashboardController : Controller
     {
-        // GET: Dashboard
+        KidKinderContext context = new KidKinderContext();
         public ActionResult Index()
         {
+            //Branşı resim olan öğretmen sayısı
+
+            int branchId = context.Branches.Where(x => x.Name == "Resim").Select(y => y.BranchId).FirstOrDefault();
+            ViewBag.resimTeacherCount = context.Teachers.Where(x => x.BranchId == branchId).Count();
+
+            //Eğitimlerin orlama ücreti
+            ViewBag.avgClassroomPrice = context.ClassRooms.Average(x => x.Price).ToString("0.00");
+
             return View();
         }
     }
